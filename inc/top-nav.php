4c5fed74dd63d9b90af919781e4fc45342c73b2f
Bradley Prasuhn <bradprasuhn@hotmail.com>
2014-02-18 18:51:36 -0600

    Separate php includes files

--- inc/top-nav.php
+++ inc/top-nav.php
@@ -2,8 +2,9 @@
     <div class="small-12 columns">
       <nav class="nav">
         <ul>
-          <li><a href="index.php">About Wild Wood</a></li>
-          <li><a href="about.php">Products</a></li>
+          <li><a href="index.php">About Us</a></li>
+          <li><a href="about.php">Portfolio</a></li>
+          <li><a href="about.php">For Sale</a></li>
           <li><a href="contact.php">Contact Us</a></li>
         </ul>
       </nav>
